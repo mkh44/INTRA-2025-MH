314333b83452fe87cff70a3768d555f05f1899d9
Molly <molly.hurley24@mail.dcu.ie>
2025-02-21 15:35:59 +0000

    Converted wavelength units to m in initial definition. Changed absorption spectrum to function

--- Photon counting non-linear spectrum.py
+++ Photon counting non-linear spectrum.py
@@ -3,19 +3,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Get wavelength values between 400 and 800
-wavelength = np.linspace(400, 800, 100)
+wavelength = np.linspace(400, 800, 100) * 1e-9
 
 # Define non-linear blackbody source spectrum
 def planck_law(wavelength, temperature):
     h = 6.626e-34 # Planck's constant (Js)
     c = 2.997e8 # Speed of light (m/s)
     k = 1.381e-23 # Boltzmann constant (J/K)
-    wavelength_m = wavelength * 1e-9 # Converting nm to m
 
-    return (2*h*c**2) / (wavelength_m**5) / (np.exp(h*c / wavelength_m * k * temperature) - 1)
-
-# Generate wavelength values
-wavelength = np.linspace(400, 800, 100)
+    return (2*h*c**2) / (wavelength**5) / (np.exp(h*c / wavelength * k * temperature) - 1)
 
 # Define blackbody spectrum for a given temperature
 temperature =5800
@@ -25,7 +21,9 @@ source_spectrum = planck_law(wavelength, temperature)
 source_spectrum /= np.sum(source_spectrum)
 
 # Make atmospheric absorption spectrum
-absorption_spectrum = 0.5 + 0.4 * np.sin((wavelength - 400) * np.pi / 200)
+def absorption_spectrum(wavelength, temperature):
+
+    return 0.5 + 0.4 * np.sin((wavelength - 400) * np.pi / 200)
 
 #Add absorption bands maybe (Gaussian)
 
