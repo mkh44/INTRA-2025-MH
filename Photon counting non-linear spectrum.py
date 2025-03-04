05f119696f5c88096781f5d1131487beb81123f0
Molly <molly.hurley24@mail.dcu.ie>
2025-03-04 14:43:25 +0000

    fixed calling of absorption spectrum

--- Photon counting non-linear spectrum.py
+++ Photon counting non-linear spectrum.py
@@ -3,20 +3,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Get wavelength values between 400 and 800. Kept in nm to avoid float result being used in absorption_spectrum calculation.
+
 wavelength = np.linspace(400, 800, 100)
+temperature = 5800
 
-#test to see if it goes to the changed name location
-
-# Define non-linear blackbody source spectrum
+# Define non-linear blackbody source spectrum function
 def planck_law(wavelength, temperature):
     h = 6.626e-34 # Planck's constant (Js)
     c = 2.997e8 # Speed of light (m/s)
     k = 1.381e-23 # Boltzmann constant (J/K)
 
-    return (2*h*c**2) / (np.exp(h*c / (wavelength * 1e-9) * k * temperature) - 1) ((wavelength * 1e-9)**5)
+    return (2*h*c**2) / ((wavelength * 1e-9)**5 * (np.exp(h*c / (wavelength *1e-9 / (k * temperature))) -1))
 
-# Define blackbody spectrum for a given temperature
-temperature = 5800
+# Create blackbody spectrum for a given temperature
 source_spectrum = planck_law(wavelength, temperature)
 
 # Normalise source spectrum
@@ -30,19 +29,19 @@ def absorption_spectrum(wavelength):
 #Add absorption bands maybe (Gaussian)
 
 # Work out observed spectrum = of source_spectrum X atmospheric_absorption
-observed_spectrum = source_spectrum * (1 - absorption_spectrum)
+observed_spectrum = source_spectrum * (1 - absorption_spectrum(wavelength))
 
 # Create DataFrame
 spectral_data = pd.DataFrame({
     'Wavelength (nm)': wavelength,
     'Source Spectrum': source_spectrum,
-    'Atmospheric Absorption': absorption_spectrum,
+    'Atmospheric Absorption': absorption_spectrum (wavelength),
     'Observed Spectrum': observed_spectrum})
 
 # Plot data
 plt.figure(figsize=(10, 6))
 line1, = plt.plot(wavelength, source_spectrum, linestyle='--', label='Source Spectrum')
-line2, = plt.plot(wavelength, absorption_spectrum, linestyle='-.', label='Atmospheric Absorption')
+line2, = plt.plot(wavelength, absorption_spectrum(wavelength), linestyle='-.', label='Atmospheric Absorption')
 line3, = plt.plot(wavelength, observed_spectrum, linewidth=2, label='Observed Spectrum')
 plt.xlabel('Wavelength (nm)')
 plt.ylabel('Intensity / Absorption')
